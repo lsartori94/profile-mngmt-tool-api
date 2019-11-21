4c4338f23fed6ce73f8bd683d171ee460d512b57
FedericoBernardi <federico.bernardi@globant.com>
2019-11-21 10:20:38 -0300

    User Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UserModule } from './User/user.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UserModule]
 })
-export class AppModule {}
+export class AppModule {};
